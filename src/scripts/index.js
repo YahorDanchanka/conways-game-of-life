3b98ecbb26f25b977b151a0a2389e173b6e21bef
YahorDanchanka <danchenko.dev@yandex.by>
2021-08-27 11:47:12 +0300

    Реализована логика игры

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,34 @@
-import { Painter } from './Painter'
+import { Painter, Cell } from './Painter'
 const painter = new Painter(document.querySelector('#canvas'))
 painter.createGrid()
 
-function startGame() {}
+function startGame() {
+  const cells = []
+
+  painter.cells.forEach(cell => {
+    const nextCell = new Cell(cell.x, cell.y, cell.size, painter)
+    const paintNeighbors = cell.getPaintNeighbors().length
+
+    if (!cell.isPaint && paintNeighbors === 3) {
+      nextCell.paint()
+    } else if (cell.isPaint && (paintNeighbors === 2 || paintNeighbors === 3)) {
+      nextCell.paint()
+    } else if (cell.isPaint && (paintNeighbors < 2 || paintNeighbors > 3)) {
+      nextCell.unpaint()
+    }
+
+    cells.push(nextCell)
+  })
+
+  painter.cells = cells
+  painter.cells.forEach(cell => {
+    if (cell.isPaint) {
+      cell.paint()
+    } else {
+      cell.unpaint()
+    }
+  })
+}
 
 /** Sidebar settings */
 const cellSizeInput = document.querySelector('#cell-size')
